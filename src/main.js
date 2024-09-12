fca47e440f5fa5f3628aa6f5259d58fee3d4e15c
ViniciusCassemira <viniciuscruzc2702@gmail.com>
2024-09-12 12:36:57 -0300

    resultado do projeto do workshop

--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
+import {game} from './game.js';
+
+const newGameButtonElem = document.getElementById('new-game');
+const inputElem = document.getElementById('word');
+const errorElem = document.getElementById('error');
+const streakElem = document.getElementById('streak');
+const streakText = document.getElementById('streakText');
+
+newGameButtonElem.addEventListener('click',function(){
+    game.newGame();
+    const lettersElem = document.querySelectorAll('.square');
+
+    for (let i = 0; i < lettersElem.length; i++){
+        lettersElem[i].innerHTML = game.letters[i].toUpperCase()
+    }
+
+    inputElem.disabled = false;
+    inputElem.value = "";
+    inputElem.focus()
+    errorElem.style.visibility = 'hidden';
+    streakText.style.visibility = 'visible';
+    streakElem.innerHTML = ""
+})
+
+document.getElementById('form').addEventListener('submit',async function(event){
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const word = formData.get('word');
+
+    const isWordValid = await game.validateWord(word.toLocaleLowerCase());
+    if (isWordValid){
+        streakElem.innerHTML = game.streak;
+        errorElem.style.visibility = 'hidden';
+        inputElem.value=""
+        // inputElem.innerHTML = ""
+    }else{
+        errorElem.style.visibility = 'visible';
+    }
+})
 
